cba189c5bf5eb8af2171e338bd49ec4873c367f2
Sandeepchinnaboina0208 <sandeepchinnaboina286@gmail.com>
2025-03-16 21:01:02 +0530

    update1

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base : "/Task-Management-Application",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
